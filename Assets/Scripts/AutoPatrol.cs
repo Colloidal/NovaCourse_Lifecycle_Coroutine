1b61ea5637e074fc3fd2ddbcb5a09fb806ff0036
Colloidal <389240875@qq.com>
2019-11-10 11:15:36 +0800

    name Change

---
+++ Assets/Scripts/AutoPatrol.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoPatrol : MonoBehaviour
+{
+    public Vector3[] points;
+    public float movingSpeed;
+
+    private void Start()
+    {
+        StartCoroutine(MoveToPoint());
+    }
+
+    IEnumerator MoveToPoint()
+    {
+        while (true)
+        {
+            foreach (var eachPoint in points)
+                yield return StartCoroutine(Moving(eachPoint));
+        }
+    }
+    IEnumerator Moving(Vector3 destination)
+    {
+        while (transform.position != destination)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, destination, movingSpeed * Time.deltaTime);
+            yield return 0;
+        }
+    }
+}
