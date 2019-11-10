cd36969af18bff460cdaf7c218d3d11691178f93
Colloidal <389240875@qq.com>
2019-11-10 10:57:26 +0800

    add testScene

---
+++ Assets/Scripts/CoroutineTest.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoroutineTest : MonoBehaviour
+{
+    private Coroutine para;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(ActiveByMethod(1,2));
+        StartCoroutine("ActiveByName",1);
+        para = StartCoroutine(ActiveByParameter(1,2));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            StopCoroutine(ActiveByMethod(0,0));
+            StopCoroutine("ActiveByName");
+            StopCoroutine(para);
+        }
+    }
+
+    IEnumerator ActiveByMethod(int first, int second)
+    {
+        while (true)
+        {
+            Debug.Log("由函数启动的协程在运行，输入值分别为" + first + "&" + second);
+            yield return new WaitForSeconds(1);
+        }
+    }
+
+    IEnumerator ActiveByName(int first)
+    {
+        while (true)
+        {
+            Debug.Log("由名字字符串启动的协程在运行，输入值分别为" + first);
+            yield return new WaitForSeconds(1);
+        }
+    }
+
+    IEnumerator ActiveByParameter(int first, int second)
+    {
+        while (true)
+        {
+            Debug.Log("由变量启动的协程在运行，输入值分别为" + first + "&" + second);
+            yield return new WaitForSeconds(1);
+        }
+    }
+}
---
+++ Assets/Scripts/LifectcleTest.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LifectcleTest : MonoBehaviour
+{
+    private void Awake()
+    {
+        Debug.Log("Awake!!!");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Start!!!");
+    }
+
+    private void FixedUpdate()
+    {
+        Debug.Log("FixedUpdate!!!");
+    }
+
+    void Update()
+    {
+        Debug.Log("Update!!!");
+    }
+
+    private void LateUpdate()
+    {
+        Debug.Log("LateUpdate!!!");
+    }
+
+
+}
